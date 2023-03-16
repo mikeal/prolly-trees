49ae210557f91eb649cc6a2609c7efefaf83ba7d
Chris Anderson <jchris@gmail.com>
2023-03-15 18:10:45 -0700

    almost works ai code

--- test/test-map.js
+++ test/test-map.js
@@ -360,10 +360,9 @@ describe('map', () => {
     })
     same(result, 1)
 
-    // const randFun = mulberry32(1)
     const errors = []
     const limit = 500
-    for (let rowCount = 0; rowCount < limit; rowCount++) {
+    for (let rowCount = 33; rowCount < limit; rowCount++) {
       const key = String.fromCharCode(rowCount)
       const value = `${rowCount}-${key}`
       // console.log('writing', key, value)
@@ -393,6 +392,364 @@ describe('map', () => {
     )
     same(errors.length, 0)
   })
+  it('load non-existent key', async () => {
+    const { get, put } = storage()
+    let root
+    for await (const node of create({ get, compare, list, ...opts })) {
+      await put(await node.block)
+      root = node
+    }
+
+    try {
+      await root.get('non_existent_key')
+      throw new Error('Should not reach this line')
+    } catch (error) {
+      same(error.message, 'Not found')
+    }
+  })
+  it('delete multiple entries', async () => {
+    const { get, put } = storage()
+    let root
+
+    for await (const node of create({ get, compare, list, ...opts })) {
+      await put(await node.block)
+      root = node
+    }
+
+    const keysToDelete = ['b', 'cc', 'z']
+    const bulk = keysToDelete.map(key => ({ key, del: true }))
+    const { blocks, root: updatedRoot, previous } = await root.bulk(bulk)
+    await Promise.all(blocks.map(block => put(block)))
+
+    // Verify the deleted keys and their values in the 'previous' result
+    const deletedKeys = previous.map(({ key }) => key)
+    const deletedValues = previous.map(({ value }) => value)
+    same(deletedKeys.sort(), keysToDelete.sort())
+    same(deletedValues, [1, 2, 1])
+
+    // Verify the deleted keys are not present in the updated tree
+    for (const key of keysToDelete) {
+      let threw = false
+      try {
+        await updatedRoot.get(key)
+      } catch (e) {
+        if (e.message === 'Not found') threw = true
+      }
+      same(threw, true)
+    }
+
+    // Get the remaining keys after deletion and sort them
+    const { result: entries } = await updatedRoot.getEntries(['a', 'zz'])
+    const remainingKeys = entries.map(({ key }) => key)
+
+    remainingKeys.sort()
+
+    // Verify the remaining keys are as expected after deletion, also sort the expected keys
+    same(remainingKeys.sort(compare), ['a', 'zz'].sort(compare))
+  })
+  it('insert causes chunker to return true for non-empty bulk', async () => {
+    const { get, put } = storage()
+    let root
+
+    for await (const node of create({ get, compare, list, ...opts })) {
+      await put(await node.block)
+      root = node
+    }
+
+    // Custom chunker function to force a split on a non-empty bulk
+    const chunker = async (node, context) => {
+      if (context.height === 1 && node.bulk.length > 0) {
+        return true
+      }
+      return false
+    }
+
+    // Insert a new key-value pair that will cause the chunker to return true
+    const keyToInsert = 'x'
+    const valueToInsert = 42
+    const { blocks, root: updatedRoot, previous } = await root.bulk([{ key: keyToInsert, value: valueToInsert }], { chunker })
+    await Promise.all(blocks.map(block => put(block)))
+
+    // Verify the previous result is empty, as it's a new insertion
+    same(previous, [])
+
+    // Verify the new key-value pair is present in the updated tree
+    const { result } = await updatedRoot.get(keyToInsert)
+    same(result, valueToInsert)
+
+    // Verify the remaining keys are still present in the updated tree
+    for (const { key, value } of list) {
+      const { result } = await updatedRoot.get(key)
+      same(result, value)
+    }
+  })
+  it('delete multiple entries within range', async () => {
+    const updatedList = [
+      { key: 'a', value: 1 },
+      { key: 'b', value: 1 },
+      { key: 'bb', value: 2 },
+      { key: 'c', value: 1 },
+      { key: 'cc', value: 2 },
+      { key: 'd', value: -1 },
+      { key: 'ff', value: 2 },
+      { key: 'h', value: 1 },
+      { key: 'z', value: 1 },
+      { key: 'zz', value: 2 }
+    ]
+
+    const { get, put } = storage()
+    let root
+
+    for await (const node of create({ get, compare, list: updatedList, ...opts })) {
+      await put(await node.block)
+      root = node
+    }
+
+    const keysToDelete = ['b', 'cc', 'd', 'z']
+    const bulk = keysToDelete.map(key => ({ key, del: true }))
+    const { blocks, root: updatedRoot, previous } = await root.bulk(bulk)
+    await Promise.all(blocks.map(block => put(block)))
+
+    previous.sort((a, b) => compare(a.key, b.key))
+
+    // Verify the deleted keys and their values in the 'previous' result
+    const deletedKeys = previous.map(({ key }) => key)
+    const deletedValues = previous.map(({ value }) => value)
+    same(deletedKeys.sort(), keysToDelete.sort())
+    same(deletedValues, [1, 2, -1, 1])
+
+    // Verify the deleted keys are not present in the updated tree
+    for (const key of keysToDelete) {
+      let threw = false
+      try {
+        await updatedRoot.get(key)
+      } catch (e) {
+        if (e.message === 'Not found') threw = true
+      }
+      same(threw, true)
+    }
+
+    // Get the remaining keys after deletion and sort them
+    const { result: entries } = await updatedRoot.getAllEntries()
+    const remainingKeys = entries.map(({ key }) => key)
+
+    remainingKeys.sort(compare)
+
+    // Verify the remaining keys are as expected after deletion, also sort the expected keys
+    same(remainingKeys, ['a', 'bb', 'c', 'ff', 'h', 'zz'])
+  })
+  it('delete multiple entries with some outside leaf range', async () => {
+    const { get, put } = storage()
+    let root
+
+    for await (const node of create({ get, compare, list, ...opts })) {
+      await put(await node.block)
+      root = node
+    }
+
+    // Include keys that are outside the range of the leaf's entryList
+    const keysToDelete = ['b', 'cc', 'z', 'xxx']
+    const bulk = keysToDelete.map(key => ({ key, del: true }))
+    const { blocks, root: updatedRoot, previous } = await root.bulk(bulk)
+    await Promise.all(blocks.map(block => put(block)))
+
+    // Verify the deleted keys and their values in the 'previous' result
+    const deletedKeys = previous.map(({ key }) => key)
+    const deletedValues = previous.map(({ value }) => value)
+    same(deletedKeys.sort(), ['b', 'cc', 'z'].sort())
+    same(deletedValues, [1, 2, 1])
+
+    // Verify the deleted keys are not present in the updated tree
+    for (const key of keysToDelete) {
+      let threw = false
+      try {
+        await updatedRoot.get(key)
+      } catch (e) {
+        if (e.message === 'Not found') threw = true
+      }
+      same(threw, true)
+    }
+
+    // Get the remaining keys after deletion and sort them
+    const { result: entries } = await updatedRoot.getEntries(['a', 'zz'])
+    const remainingKeys = entries.map(({ key }) => key)
+
+    remainingKeys.sort()
+
+    // Verify the remaining keys are as expected after deletion, also sort the expected keys
+    same(remainingKeys.sort(compare), ['a', 'zz'].sort(compare))
+  })
+  it('inserts with custom chunker that triggers branch for uninitialized node', async () => {
+    const { get, put } = storage()
+    let root
+
+    for await (const node of create({ get, compare, list, ...opts })) {
+      await put(await node.block)
+      root = node
+    }
+
+    // Custom chunker that inserts new entries with keys smaller than existing entries
+    const customChunker = async (entry, distance) => {
+      return compare(entry.key, 'a') < 0
+    }
+
+    const keysToInsert = ['-1', '-2', '-3']
+    const bulk = keysToInsert.map(key => ({ key, value: -1 }))
+
+    const { blocks, root: updatedRoot } = await root.bulk(bulk, { ...opts, chunker: customChunker })
+    await Promise.all(blocks.map(block => put(block)))
+
+    // Verify the inserted keys and their values
+    for (const key of keysToInsert) {
+      const value = await updatedRoot.get(key)
+      same(value, -1)
+    }
+
+    // Get the existing keys after insertion and sort them
+    const { result: entries } = await updatedRoot.getEntries(['-3', 'zz'])
+    const resultingKeys = entries.map(({ key }) => key)
+
+    resultingKeys.sort(compare)
+
+    // Verify the resulting keys are as expected after insertion, also sort the expected keys
+    const expectedKeys = ['a', 'b', 'cc', 'ff', 'h', 'z', '-1', '-2', '-3'].sort(compare)
+    same(resultingKeys, expectedKeys)
+  })
+
+  it('insert multiple entries within range causing chunker function to return true', async () => {
+    const initialList = [
+      { key: 'a', value: 1 },
+      { key: 'b', value: 1 },
+      { key: 'bb', value: 2 },
+      { key: 'c', value: 1 },
+      { key: 'cc', value: 2 },
+      { key: 'd', value: -1 },
+      { key: 'ff', value: 2 },
+      { key: 'h', value: 1 },
+      { key: 'z', value: 1 },
+      { key: 'zz', value: 2 }
+    ]
+
+    const { get, put } = storage()
+    let root
+
+    // Create initial tree
+    for await (const node of create({ get, compare, list: initialList, ...opts })) {
+      await put(await node.block)
+      root = node
+    }
+
+    // Inserts that fall within the range of the tree
+    const bulkInserts = [
+      { key: 'aa', value: 3 },
+      { key: 'ca', value: 4 },
+      { key: 'da', value: 5 }
+    ]
+
+    // Custom chunker function that returns true for a non-empty bulk
+    const customChunker = async (entry, distance) => {
+      return distance > 0
+    }
+
+    // Perform bulk insert
+    const { blocks, root: newRoot } = await root.bulk(bulkInserts, { ...opts, chunker: customChunker })
+
+    // Save the new blocks
+    await Promise.all(blocks.map((block) => put(block)))
+
+    // Assert the new entries have been inserted correctly
+    const _get = async (k) => (await newRoot.get(k)).result
+    same(await _get('aa'), 3)
+    same(await _get('ca'), 4)
+    same(await _get('da'), 5)
+
+    // Assert the original entries remain unchanged
+    const expected = [
+      ['a', 1],
+      ['aa', 3],
+      ['b', 1],
+      ['bb', 2],
+      ['c', 1],
+      ['ca', 4],
+      ['cc', 2],
+      ['d', -1],
+      ['da', 5],
+      ['ff', 2],
+      ['h', 1],
+      ['z', 1],
+      ['zz', 2]
+    ]
+
+    for (const [key, value] of expected) {
+      same(await _get(key), value)
+    }
+
+    // Assert the tree's structure remains valid
+    const { result: allEntries } = await newRoot.getAllEntries()
+    const actualKeys = allEntries.map((entry) => entry.key)
+    const expectedKeys = expected.map(([key]) => key)
+    same(actualKeys, expectedKeys)
+  })
+  it('inserts with custom chunker that triggers uncovered branches', async () => {
+    const { get, put } = storage()
+    let root
+
+    for await (const node of create({ get, compare, list, ...opts })) {
+      await put(await node.block)
+      root = node
+    }
+
+    // Custom chunker function that triggers specific branches
+    const customChunker = async (entry, distance) => {
+      return distance > 1
+    }
+
+    // Bulk inserts that will trigger the branches
+    const bulkInserts = [
+      { key: 'a', value: 10 },
+      { key: 'c', value: 20 },
+      { key: 'h', value: 30 },
+      { key: 'z', value: 40 }
+    ]
+
+    // Perform bulk insert
+    const { blocks, root: newRoot } = await root.bulk(bulkInserts, { ...opts, chunker: customChunker })
+
+    // Save the new blocks
+    await Promise.all(blocks.map((block) => put(block)))
+
+    // Assert the new entries have been inserted correctly
+    const _get = async (k) => (await newRoot.get(k)).result
+    same(await _get('a'), 10)
+    same(await _get('c'), 20)
+    same(await _get('h'), 30)
+    same(await _get('z'), 40)
+
+    // Assert the other entries remain unchanged
+    const expected = [
+      ['a', 10],
+      ['b', 1],
+      ['bb', 2],
+      ['c', 20],
+      ['cc', 2],
+      ['d', 1],
+      ['ff', 2],
+      ['h', 30],
+      ['z', 40],
+      ['zz', 2]
+    ]
+
+    for (const [key, value] of expected) {
+      same(await _get(key), value)
+    }
+
+    // Assert the tree's structure remains valid
+    const { result: allEntries } = await newRoot.getAllEntries()
+    const actualKeys = allEntries.map((entry) => entry.key)
+    const expectedKeys = expected.map(([key]) => key)
+    same(actualKeys, expectedKeys)
+  })
+
   it.skip('big map', async () => {
     const { get, put } = storage()
     let mapRoot
